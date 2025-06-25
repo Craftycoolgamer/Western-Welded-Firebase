776795a7fae3bb672be0a91cb760be80cdde1a37
Quinn Prisbrey <craftycoolgamer@gmail.com>
2025-06-25 11:19:00 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { getDatabase, ref, onValue } from "firebase/database";
+import { app, auth, db } from './configuration'; 
 
 function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Reference to your data path
+    const dbRef = ref(db, "/");
+
+    // Function to fetch data from the database
+    const fetchData = () => {
+      // Listen for changes in the collection
+      onValue(dbRef, (snapshot) => {
+        const dataItem = snapshot.val();
+
+        // Check if dataItem exists
+        if (dataItem) {
+          // Convert the object values into an array
+          const displayItem = Object.values(dataItem);
+          setData(displayItem);
+        }
+      });
+    };
+
+    // Fetch data when the component mounts
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Data from database:</h1>
+      <ul>
+        {data.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
